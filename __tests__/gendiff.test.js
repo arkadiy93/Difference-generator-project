1d95b576bd3a3245db91bfee5583de79780e60df
Arkadiy Stepanov <nvriper@gmail.com>
2018-04-04 09:19:13 +0200

    add yaml parser

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,9 +1,15 @@
-import fs from 'fs';
 import path from 'path';
+import yaml from 'js-yaml';
+import fs from 'fs';
 import genDiff from '../src/genDiff';
 
 
 test('find changes between json files', () => {
   expect(JSON.stringify(genDiff('__tests__/__fixtures__/js1.json', '__tests__/__fixtures__/js2.json')))
     .toBe((fs.readFileSync(path.join(process.cwd(), '__tests__/__fixtures__/genDiffSolution.txt'), 'utf8')));
+});
+
+test('find changes between yaml files', () => {
+  expect(yaml.toString(genDiff('__tests__/__fixtures__/yaml1.yml', '__tests__/__fixtures__/yaml2.yml')))
+    .toBe(yaml.toString(fs.readFileSync(path.join(process.cwd(), '__tests__/__fixtures__/genDiffSolution.txt'), 'utf8')));
 });
