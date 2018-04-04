16e78469d2d95c9b1c02faac70d420d783d3342f
Arkadiy Stepanov <nvriper@gmail.com>
2018-04-04 18:32:13 +0200

    add yaml test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -6,3 +6,8 @@ test('find changes between json files', () => {
   expect((genDiff('__tests__/__fixtures__/js1.json', '__tests__/__fixtures__/js2.json')))
     .toBe((fs.readFileSync(path.join(__dirname, '__fixtures__/genDiffSolution.txt'), 'utf8')));
 });
+
+test('find changes between yaml files', () => {
+  expect((genDiff('__tests__/__fixtures__/yaml1.yml', '__tests__/__fixtures__/yaml2.yml')))
+    .toBe((fs.readFileSync(path.join(__dirname, '__fixtures__/genDiffSolution.txt'), 'utf8')));
+});
