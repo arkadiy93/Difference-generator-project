b47b9aa3d61359ae83159497a8e21d172a4a4838
Arkadiy Stepanov <nvriper@gmail.com>
2018-04-05 03:28:16 +0200

    refactor index

--- src/index.js
+++ src/index.js
@@ -8,11 +8,9 @@ const toString = (config1, config2) => {
   const dataInString = allConfigKeys.reduce((acc, el) => {
     if (!has(config1, el)) {
       return `${acc}\n  + ${el}: ${config2[el]}`;
-    }
-    if (!has(config2, el)) {
+    } else if (!has(config2, el)) {
       return `${acc}\n  - ${el}: ${config1[el]}`;
-    }
-    if (config1[el] === config2[el]) {
+    } else if (config1[el] === config2[el]) {
       return `${acc}\n    ${el}: ${config1[el]}`;
     }
     return `${acc}\n  + ${el}: ${config2[el]}\n  - ${el}: ${config1[el]}`;
