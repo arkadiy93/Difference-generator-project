f8215c0fbcc1fd582c677b60f22b895e2750c630
Arkadiy Stepanov <nvriper@gmail.com>
2018-04-05 03:25:07 +0200

    refactor index

--- src/index.js
+++ src/index.js
@@ -3,20 +3,19 @@ import path from 'path';
 import fs from 'fs';
 import getParser from './parsers';
 
-
 const toString = (config1, config2) => {
   const allConfigKeys = union(keys(config1), keys(config2));
   const dataInString = allConfigKeys.reduce((acc, el) => {
-    if (has(config1, el) && has(config2, el)) {
-      if (config1[el] === config2[el]) {
-        return `${acc}\n    ${el}: ${config1[el]}`;
-      }
-      return `${acc}\n  + ${el}: ${config2[el]}\n  - ${el}: ${config1[el]}`;
+    if (!has(config1, el)) {
+      return `${acc}\n  + ${el}: ${config2[el]}`;
     }
-    if (has(config1, el)) {
+    if (!has(config2, el)) {
       return `${acc}\n  - ${el}: ${config1[el]}`;
     }
-    return `${acc}\n  + ${el}: ${config2[el]}`;
+    if (config1[el] === config2[el]) {
+      return `${acc}\n    ${el}: ${config1[el]}`;
+    }
+    return `${acc}\n  + ${el}: ${config2[el]}\n  - ${el}: ${config1[el]}`;
   }, '{');
   return `${dataInString}\n}`;
 };
@@ -34,6 +33,5 @@ const genDiff = (firstConfig, secondConfig) => {
   const config2 = parser(data2);
   return toString(config1, config2);
 };
-
 
 export default genDiff;
